eeaa53ba691d0a4bbad3bc99f5e0c887dbc26c85
SeraphJACK <seraphjack@outlook.com>
2023-03-17 10:50:35 +0800

    Add IPv6 support

--- usr/local/bin/dn42_peer_up.sh
+++ usr/local/bin/dn42_peer_up.sh
@@ -8,5 +8,6 @@ source /etc/dn42/peers/$PEER
 wg-quick up $WG_INTERFACE
 ip link set $WG_INTERFACE netns dn42
 
-ip netns exec dn42 ip addr add $ROUTER_IP peer $PEER_IP dev $WG_INTERFACE
 ip netns exec dn42 ip link set $WG_INTERFACE up
+ip netns exec dn42 ip addr add $ROUTER_IP peer $PEER_IP dev $WG_INTERFACE
+ip netns exec dn42 ip addr add $LOCAL_IP_V6 peer $PEER_IP_V6 dev $WG_INTERFACE
